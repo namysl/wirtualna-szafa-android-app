02b10e4b1f0e69245c7e35ab86d5c46efbe953d4
namysl <e.k.namysl@gmail.com>
2022-03-08 18:52:03 +0100

    pierwsza testowa apka i aforyzm dnia

---
+++ 02_aforyzmy/MyFirstApp/aforyzmy/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        java.util.Random gen=new java.util.Random();
+
+        String[] tab = getResources().getStringArray(R.array.tab_aforyzmy);
+        int i = gen.nextInt(tab.length);
+
+        final TextView TextView = findViewById(R.id.textView1);
+
+        String message = tab[i];
+        TextView.setText(message);
+
+    }
+}
+
+
+
