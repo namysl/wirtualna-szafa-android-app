86212baf98674ebce703eee2d891e18b15a391dc
namysl <e.k.namysl@gmail.com>
2022-03-12 15:33:16 +0100

    added strings and basic buttons

--- 03_kalkulator/Kalkulator/app/src/main/java/com/example/kalkulator/MainActivity.java
+++ 03_kalkulator/Kalkulator/app/src/main/java/com/example/kalkulator/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.kalkulator;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void nie_wiem(){
+
+        TextView obj = (TextView)findViewById(R.id.textView);
+        CharSequence cs = obj.getText();
+        String s = cs.toString();
+        double x = Double.parseDouble(s);
+        x = x*x;
+        String s2 = Double.toString(x);
+        obj.setText(s2);
+    }
+
+    public void cyfra1(View view){
+        TextView obj = (TextView)findViewById(R.id.textView);
+        
+        String text = "1";
+        obj.setText(text);
+    }
+
+    public void usun(View view){
+        String text = "";
+        TextView obj = (TextView)findViewById(R.id.textView);
+        obj.setText(text);
+    }
+
 }
