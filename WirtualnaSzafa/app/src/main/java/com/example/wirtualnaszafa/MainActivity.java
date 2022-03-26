2109d9a6c4aa481c29b7ec4dfc963692bead2285
namysl <e.k.namysl@gmail.com>
2022-03-26 20:12:50 +0100

    fixed nav fragments not opening in the menu

--- WirtualnaSzafa/app/src/main/java/com/example/wirtualnaszafa/MainActivity.java
+++ WirtualnaSzafa/app/src/main/java/com/example/wirtualnaszafa/MainActivity.java
@@ -41,7 +41,9 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_add_element)
+                R.id.nav_home, R.id.nav_gallery,
+                R.id.nav_add_element, R.id.nav_remove_element,
+                R.id.nav_randomize, R.id.nav_collections)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
