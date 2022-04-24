f27e9847013ae619eda7b4d0521bbe514a3126ff
namysl <e.k.namysl@gmail.com>
2022-04-25 00:20:48 +0200

    deleted copy

--- API_from_AP/Apka/app/src/main/java/com/example/wirtualnaszafa/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.wirtualnaszafa;
-
-public class Constants {
-    public static final String TOKEN_KEY = "com.example.wirtualnaszafa.TOKEN_KEY";
-    public static final String ACCOUNT_PREFERENCES_KEY = "com.example.wirtualnaszafa.ACCOUNT_PREFERENCES_KEY";
-}
--- API_from_AP/Apka/app/src/main/java/com/example/wirtualnaszafa/ui/gallery/GalleryViewModel.java
+++
@@ -1,4 +0,0 @@
-package com.example.wirtualnaszafa.ui.gallery;
-
-public class GalleryViewModel {
-}
