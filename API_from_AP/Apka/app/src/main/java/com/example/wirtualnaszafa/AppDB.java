f115523f17b6c1abf88670db9f524fbb35ad381a
namysl <e.k.namysl@gmail.com>
2022-12-21 22:51:33 +0100

    rm dir

--- API_from_AP/Apka/app/src/main/java/com/example/wirtualnaszafa/AppDB.java
+++
@@ -1,9 +0,0 @@
-package com.example.wirtualnaszafa;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {WardrobeDB.class}, version = 1)
-public abstract class AppDB extends RoomDatabase {
-    public abstract WardrobeDAO wardrobeDAO();
-}
--- API_from_AP/Apka/app/src/main/java/com/example/wirtualnaszafa/ui/randomize/RandomizeViewModel.java
+++
@@ -1,4 +0,0 @@
-package com.example.wirtualnaszafa.ui.randomize;
-
-public class RandomizeViewModel {
-}
